2b16359290ce22132cda0ca1f7797b444de9515b
Curious T!mo <36850613+CuriousTimo@users.noreply.github.com>
2025-06-29 07:37:43 +0200

    Update LocoNet_Railcom_Display.ino
    Code cleanup and LiquidCrystal_I2C library swap

--- LocoNet_Railcom_Display/LocoNet_Railcom_Display.ino
+++ LocoNet_Railcom_Display/LocoNet_Railcom_Display.ino
@@ -1,130 +1,131 @@
+/************************************************************************************************************
+ *
+ *  Copyright (C) 2015-2016 Timo Sariwating
+ *
+ *  This program is free software under GNU GPL v3 or later.
+ *
+ *  Description:
+ *  LocoNet Packet Monitor
+ *  Demonstrates usage of LocoNet.processSwitchSensorMessage()
+ *  and the various notifyXXXXXXX callback functions.
+ *
+ *  Hardware:
+ *  - Arduino with LocoNet interface
+ *  - LCD connected via I2C using John Rickman's LiquidCrystal_I2C library
+ *
+ *  Libraries:
+ *  - LocoNet: https://github.com/mrrwa/LocoNet
+ *  - LiquidCrystal_I2C: https://github.com/johnrickman/LiquidCrystal_I2C
+ *
+ ************************************************************************************************************/
+
 #include <LocoNet.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-// LocoNet Packet Monitor
-// Demonstrates the use of the:
-//
-//   LocoNet.processSwitchSensorMessage(LnPacket)  
-//
-// function and examples of each of the notifyXXXXXXX user call-back functions
+// Using John Rickman's LiquidCrystal_I2C library constructor
+LiquidCrystal_I2C lcd(0x20, 16, 2);
 
-LiquidCrystal_I2C lcd(0x20, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-
-lnMsg        *LnPacket;
+lnMsg* LnPacket = nullptr;
 
 void setup() {
-  // First initialize the LocoNet interface
   LocoNet.init();
 
-  // Configure the serial port for 57600 baud
   Serial.begin(57600);
-  Serial.println("LocoNet Monitor");
+  Serial.println(F("LocoNet Monitor"));
 
-  lcd.begin(16,2);               // initialize the lcd
+  lcd.init();        // Initialize the lcd
+  lcd.backlight();   // Turn on backlight
   lcdPrint("Loconet", "Railcom Monitor");
 }
 
-void loop() {  
-  // Check for any received LocoNet packets
-  LnPacket = LocoNet.receive() ;
-  if( LnPacket ) {
+void loop() {
+  LnPacket = LocoNet.receive();
+  if (LnPacket) {
+    uint8_t opcode = LnPacket->sz.command;
 
-    unsigned char opcode = (int)LnPacket->sz.command;
-
-    if (opcode == OPC_GPON)  {        // GLOBAL power ON request 0x83     
-      lcdPrint("GLOBAL power ON", "");
-    } else if (opcode == OPC_GPOFF) { // GLOBAL power OFF req 0x82
-      lcdPrint("GLOBAL power OFF", "");
+    switch (opcode) {
+      case OPC_GPON:
+        lcdPrint("GLOBAL power ON", "");
+        break;
+      case OPC_GPOFF:
+        lcdPrint("GLOBAL power OFF", "");
+        break;
+      default:
+        break;
     }
-    
-      // First print out the packet in HEX
-    Serial.print("RX: ");
-    uint8_t msgLen = getLnMsgSize(LnPacket); 
-    for (uint8_t x = 0; x < msgLen; x++)
-    {
-      uint8_t val = LnPacket->data[x];
-        // Print a leading 0 if less than 16 to make 2 HEX digits
-      if(val < 16)
-        Serial.print('0');
-        
+
+    Serial.print(F("RX: "));
+    uint8_t msgLen = getLnMsgSize(LnPacket);
+    for (uint8_t i = 0; i < msgLen; ++i) {
+      uint8_t val = LnPacket->data[i];
+      if (val < 0x10) Serial.print('0');  // Leading zero for hex format
       Serial.print(val, HEX);
       Serial.print(' ');
     }
-    
-      // If this packet was not a Switch or Sensor Message then print a new line 
-    if(!LocoNet.processSwitchSensorMessage(LnPacket)) {
+
+    if (!LocoNet.processSwitchSensorMessage(LnPacket)) {
       Serial.println();
     }
   }
 }
 
-void lcdPrint(String line1, String line2) {
+void lcdPrint(const String& line1, const String& line2) {
   lcd.clear();
-  lcd.home ();
+  lcd.setCursor(0, 0);
   lcd.print(line1);
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print(line2);
 }
 
-  // This call-back function is called from LocoNet.processSwitchSensorMessage
-  // for all Sensor messages
-void notifySensor( uint16_t Address, uint8_t State ) {
-  Serial.print("Sensor: ");
-  Serial.print(Address, DEC);
-  Serial.print(" - ");
-  Serial.println( State ? "Active" : "Inactive" );
+// Callback functions called by LocoNet.processSwitchSensorMessage()
+
+void notifySensor(uint16_t address, uint8_t state) {
+  Serial.print(F("Sensor: "));
+  Serial.print(address);
+  Serial.print(F(" - "));
+  Serial.println(state ? F("Active") : F("Inactive"));
 }
 
-  // This call-back function is called from LocoNet.processSwitchSensorMessage
-  // for all Switch Request messages
-void notifySwitchRequest( uint16_t Address, uint8_t Output, uint8_t Direction ) {
-  Serial.print("Switch Request: ");
-  Serial.print(Address, DEC);
+void notifySwitchRequest(uint16_t address, uint8_t output, uint8_t direction) {
+  Serial.print(F("Switch Request: "));
+  Serial.print(address);
   Serial.print(':');
-  Serial.print(Direction ? "Closed" : "Thrown");
-  Serial.print(" - ");
-  Serial.println(Output ? "On" : "Off");
+  Serial.print(direction ? F("Closed") : F("Thrown"));
+  Serial.print(F(" - "));
+  Serial.println(output ? F("On") : F("Off"));
 }
 
-  // This call-back function is called from LocoNet.processSwitchSensorMessage
-  // for all Switch Report messages
-void notifySwitchReport( uint16_t Address, uint8_t Output, uint8_t Direction ) {
-  Serial.print("Switch Report: ");
-  Serial.print(Address, DEC);
+void notifySwitchReport(uint16_t address, uint8_t output, uint8_t direction) {
+  Serial.print(F("Switch Report: "));
+  Serial.print(address);
   Serial.print(':');
-  Serial.print(Direction ? "Closed" : "Thrown");
-  Serial.print(" - ");
-  Serial.println(Output ? "On" : "Off");
+  Serial.print(direction ? F("Closed") : F("Thrown"));
+  Serial.print(F(" - "));
+  Serial.println(output ? F("On") : F("Off"));
 }
 
-  // This call-back function is called from LocoNet.processSwitchSensorMessage
-  // for all Switch State messages
-void notifySwitchState( uint16_t Address, uint8_t Output, uint8_t Direction ) {
-  Serial.print("Switch State: ");
-  Serial.print(Address, DEC);
+void notifySwitchState(uint16_t address, uint8_t output, uint8_t direction) {
+  Serial.print(F("Switch State: "));
+  Serial.print(address);
   Serial.print(':');
-  Serial.print(Direction ? "Closed" : "Thrown");
-  Serial.print(" - ");
-  Serial.println(Output ? "On" : "Off");
+  Serial.print(direction ? F("Closed") : F("Thrown"));
+  Serial.print(F(" - "));
+  Serial.println(output ? F("On") : F("Off"));
 }
 
-  // This call-back function is called from LocoNet.processSwitchSensorMessage
-  // for OPC_MULTI_SENSE 0xD0
-void notifyMultiSenseTransponder( uint16_t Address, uint8_t Zone, uint16_t LocoAddress, uint8_t Present ) {
-  Serial.print("Railcom Sensor ");
-  Serial.print(Address);
-  Serial.print(" reports ");
-  Serial.print(Present? "present" : "absent");
-  Serial.print(" of decoder address ");
-  Serial.print(LocoAddress, DEC);
-  Serial.print(" in zone ");
-  Serial.println(Zone, HEX);
+void notifyMultiSenseTransponder(uint16_t address, uint8_t zone, uint16_t locoAddress, uint8_t present) {
+  Serial.print(F("Railcom Sensor "));
+  Serial.print(address);
+  Serial.print(F(" reports "));
+  Serial.print(present ? F("present") : F("absent"));
+  Serial.print(F(" of decoder address "));
+  Serial.print(locoAddress);
+  Serial.print(F(" in zone "));
+  Serial.println(zone, HEX);
 
-  String line1 = "Decoder ";
-  line1 = line1 + LocoAddress;
-  String line2 = Present ? "present" : "absent";
-  line2 = line2 + " zone " + Address;
+  String line1 = "Decoder " + String(locoAddress);
+  String line2 = (present ? "present" : "absent") + String(" zone ") + String(address);
 
   lcdPrint(line1, line2);
 }
